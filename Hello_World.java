6f133ad7459dea30f7b737400505f6aa157650bf
root <root@ip-172-31-28-39.ec2.internal>
2021-05-12 05:35:41 +0000

    Branch second commit

--- Hello_World.java
+++ Hello_World.java
@@ -2,6 +2,7 @@ class Hello_World{
     public static void main(String args[]){  
      System.out.println("Hello Guys");
      System.out.println("Welcome to Devops");
-     System.out.println("New Branch added for Bug-Fix"); 
+     System.out.println("New Branch added for Bug-Fix");
+     System.out.println("Testing rebase-1"); 
     }
 }
