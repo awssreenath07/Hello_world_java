ca329cf88e2f2843323b4bca7d6e8cfb161c300f
root <root@ip-172-31-28-39.ec2.internal>
2021-05-12 05:37:01 +0000

    Branch third commit

--- Hello_World.java
+++ Hello_World.java
@@ -3,6 +3,7 @@ class Hello_World{
      System.out.println("Hello Guys");
      System.out.println("Welcome to Devops");
      System.out.println("New Branch added for Bug-Fix");
-     System.out.println("Testing rebase-1"); 
+     System.out.println("Testing rebase-1");
+     System.out.println("Testing rebase-2"); 
     }
 }
