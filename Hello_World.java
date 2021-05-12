76f784c2e0cfba4eb4cc73aa2a3b86e0654f30a1
srinath <jmstech28@gmail.com>
2021-05-12 06:16:36 +0000

    to test GUI merge

--- Hello_World.java
+++ Hello_World.java
@@ -1,9 +1,12 @@
-class Hello_World{  
-    public static void main(String args[]){  
+class Hello_World{
+    public static void main(String args[]){
      System.out.println("Hello Guys");
      System.out.println("Welcome to Devops");
      System.out.println("New Branch added for Bug-Fix");
      System.out.println("Testing rebase-1");
-     System.out.println("Testing rebase-2"); 
+     System.out.println("Testing rebase-2");
+     System.out.println("Testing git fetch still pending");
+     System.out.println("To test merge in GUI");
     }
 }
+
