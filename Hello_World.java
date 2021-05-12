997aefe69a08ac2a0b6e8d0fc9dad60739d53b95
root <root@ip-172-31-28-39.ec2.internal>
2021-05-12 02:50:01 +0000

    Second Commit

--- Hello_World.java
+++ Hello_World.java
@@ -1,5 +1,6 @@
 class Hello_World{  
     public static void main(String args[]){  
-     System.out.println("Hello Guys"); 
+     System.out.println("Hello Guys");
+     System.out.println("Welcome to Devops"); 
     }
 }
