d7b93012f4db731f79eab3b68c3f91857109c98b
root <root@ip-172-31-28-39.ec2.internal>
2021-05-12 05:48:48 +0000

    Testing git fetch

--- Hello_World.java
+++ Hello_World.java
@@ -4,6 +4,7 @@ class Hello_World{
      System.out.println("Welcome to Devops");
      System.out.println("New Branch added for Bug-Fix");
      System.out.println("Testing rebase-1");
-     System.out.println("Testing rebase-2"); 
+     System.out.println("Testing rebase-2");
+     System.out.println("Testing git fetch"); 
     }
 }
